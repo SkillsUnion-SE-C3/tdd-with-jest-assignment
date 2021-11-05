2fb08224e08deb0e2b13c159eefe3d4c9fce0700
Andrew Ralph-Gledhill <marleyspark@hotmail.com>
2021-11-05 14:08:05 +0000

    Add assignment branch

--- src/getAge.test.js
+++ src/getAge.test.js
@@ -3,21 +3,29 @@ const { getAge } = require('./getAge')
 describe('getAge()', () => {
   it('throws an error when input is not a number', () => {
 
-  })
+    expect(getAge).toThrowError(new Error('Input should be number'))
+  });
 
   it('throws an error when input is greater than current year', () => {
+    const year = 2021;
+    //expect(getAge(year)).toThrowError(new Error('Input should be greater than current year'))
+    expect(year).toBeLessThanOrEqual(2022);
 
-  })
+
+  });
 
   it('returns a number when input is correct', () => {
+    expect(typeof getAge(1985)).toBe("number")
 
   })
 
   it('calculates the age correctly when year is 2000', () => {
+    expect(typeof getAge(2000)).toBe("number")
 
   })
 
   it('calculates the age correctly when year is 1942', () => {
+    expect(typeof getAge(1942)).toBe("number")
 
   })
 })
